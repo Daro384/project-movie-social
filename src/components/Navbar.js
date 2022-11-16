091ab0612c1b7dba30fe22e7c582b6110a751cf1
Grace Durant <gdurant34@gmail.com>
2022-11-15 16:06:41 -0800

    created pages, navbar, and reactRouter

---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link, useMatch, useResolvedPath} from 'react-router-dom'
+
+const Navbar = () => {
+
+    function CustomLink({ to, children, ...props }) {
+        const resolvedPath = useResolvedPath(to)
+        const isActive = useMatch({ path: resolvedPath.pathname, end:true })
+        return (
+            <li className={isActive ? "active" : ""}>
+                <Link to={to} {...props}>{children}</Link>
+            </li>
+        )
+    }
+
+    return (
+        <nav className="nav">
+            <div className="site-title">Movies Social</div>
+            <ul>
+                <CustomLink to="/">Login</CustomLink>
+                <CustomLink to="/MyMovies">My Movies</CustomLink>
+                <CustomLink to="/Movies">Movies</CustomLink>
+                <CustomLink to="/OtherUsers">Other Users</CustomLink>
+            </ul>
+        </nav>
+    )
+};
+
+export default Navbar;
---
+++ src/pages/OtherUsers.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const OtherUsers = () => {
+    return <h1>Other Users</h1>
+};
+
+export default OtherUsers;
