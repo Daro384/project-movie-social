a1b8b9b7d1573f783adbb356f0d8d0efc2473969
Grace Durant <gdurant34@gmail.com>
2022-11-16 22:59:50 -0800

    rendered search results and cards

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,16 +1,30 @@
-import React from 'react';
-// import { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import SearchForm from '../components/SearchForm'
+import MovieCard from '../components/MovieCard';
+import { Card } from "semantic-ui-react";
+
 
 const Movies = () => {
-    // const [movies, setMovies] = useState([])
+    const [movieResults, setMovieResults] = useState({});
 
 
-    
     return (
         <div>
             <h1>Movies</h1>
-            <SearchForm />
+            <SearchForm
+                setMovieResults={setMovieResults}
+                movieResults={movieResults}
+            />
+            {/* 
+            TODO: below can be moved to a new component. 
+            Something like: MovieCollection 
+            */}
+            <Card.Group itemsPerRow={5}>
+                {movieResults.Search?.map(movie => {
+                   return <MovieCard key={`${movie.Title}-${movie.Year}`} title={movie.Title} poster={movie.Poster} year={movie.Year} />
+                })}
+                {/* <pre>{JSON.stringify(movieResults, null, 10)}</pre> */}
+            </Card.Group>
         </div>
     )
 };
