fc061398844b5b8c9e5cdd16b8bf544e92d4e4ca
Grace Durant <gdurant34@gmail.com>
2022-11-18 08:25:12 -0800

    created a new display modal for the myMovie cards

---
+++ src/components/MyMovieInfoModal.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { Button, Image, Modal } from 'semantic-ui-react'
+
+const MyMovieInfoModal = ({ open, setOpen, movie, review }) => {
+    // console.log(movie)
+
+    return (
+        <Modal open={open}>
+            <Modal.Header>{movie.title}</Modal.Header>
+            <Modal.Content image>
+                <Image size='medium' src={movie.poster} wrapped />
+                <Modal.Description>
+                    <p>Genre: {movie.genre}</p>
+                    <p>Actors: {movie.actors}</p>
+                    <p>Year: {movie.year}</p>
+                    <p>Director: {movie.director}</p>
+                    <p>Plot: {movie.plot}</p>
+                    <p>Rated: {movie.rated}</p>
+                    <p>Runtime: {movie.runtime}</p>
+                    <p>Writer: {movie.writer}</p>
+                    <p>IMDb Rating: {movie.imdb_rating}</p>
+                    <p>My Rating: {review.rating}</p>
+
+                </Modal.Description>
+                <Modal.Actions>
+                    <Button onClick={() => setOpen(false)}>Close</Button>
+                </Modal.Actions>
+            </Modal.Content>
+        </Modal>
+    )
+}
+
+export default MyMovieInfoModal;
